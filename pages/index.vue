e73961176f0d3f90bc0eaa2eb37c689e1b63f90e
atiflayer <95560421+atiflayer@users.noreply.github.com>
2022-09-01 18:12:41 +0600

    c1

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,14 @@
             <Article />
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-6">
                 <div class="py-4 border-b border-gray-700">
-                    <!-- <Article /> -->
-                    <!-- <Article /> -->
+                    <Article />
+                    <Article />
                 </div>
-                <!-- <div class="py-4 border-b border-gray-700">
+                <div class="py-4 border-b border-gray-700">
                     <Article />
                     <Article />
-                </div> -->
+                </div>
             </div>
         </div>
-        <!-- <div class="flex flex-col w-full">
-            <Article />
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-6">
-                <div class="py-4 border-b border-gray-700">
-                    <Article />
-                    <Article />
-                </div>
-                <div class="py-4 border-b border-gray-700">
-                    <Article />
-                    <Article />
-                </div>
-            </div>
-        </div> -->
     </div>
 </template>
